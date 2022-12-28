ca2664552ae13457b47a7fc278a5c0b96446dc62
kita456 <g140702@gmail.com>
2022-12-28 14:48:51 +0900

    replの初期実装

--- main.go
+++ main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/kfess/go_interpreter/repl"
+)
+
 func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
+		user.Username)
+	fmt.Printf("Fell free to type in commnads\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
--- repl/repl.go
+++ repl/repl.go
@@ -1,3 +1,27 @@
 package repl
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/kfess/go_interpreter/lexar"
+)
+
 const PROMPT = ">>"
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		l := lexar.New(line)
+		for tok := l.NextToken(); tok.Type != "EOF"; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
