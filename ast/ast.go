cf330239925333adcb65732821e60c665c8a778b
kita456 <g140702@gmail.com>
2022-12-29 19:27:14 +0900

    stringを追加

--- ast/ast.go
+++ ast/ast.go
@@ -1,11 +1,16 @@
 package ast
 
-import "github.com/kfess/go_interpreter/token"
+import (
+	"bytes"
+
+	"github.com/kfess/go_interpreter/token"
+)
 
 type Node interface {
 	// return the literal token to which the node is related,
 	// for debug and test
 	TokenLiteral() string
+	String() string
 }
 
 // 文
@@ -32,6 +37,14 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+	for _, stms := range p.Statements {
+		out.WriteString(stms.String())
+	}
+	return out.String()
+}
+
 // vvv v   vv
 // let x = 10
 type LetStatement struct {
@@ -45,6 +58,22 @@ func (ls *LetStatement) statementNode() {}
 
 func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
+}
+
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ") // let
+	out.WriteString(ls.Name.String())        // x
+	out.WriteString(" = ")                   // =
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String()) // 5
+	}
+
+	out.WriteString(";")
+
+	return out.String()
 }
 
 // /   v
@@ -61,6 +90,10 @@ func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
 }
 
+func (i *Identifier) String() string {
+	return i.Value
+}
+
 // vvvvvv vv
 // return 10
 type ReturnStatement struct {
@@ -75,6 +108,21 @@ func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal
 }
 
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
 type ExpressionStatement struct {
 	Token      token.Token
 	Expression Expression
@@ -84,4 +132,11 @@ func (es *ExpressionStatement) statementNode() {}
 
 func (es *ExpressionStatement) TokenLiteral() string {
 	return es.Token.Literal
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
 }
